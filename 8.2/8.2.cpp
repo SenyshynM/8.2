bf87e5afb5d71530c75198137741cfc1fda063ee
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2021-03-18 12:28:19 +0200

    1

---
+++ 8.2/8.2.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+bool FirstLetter(char* str);
+
+int main()
+{
+    char s[101]; 
+    cout << "Enter string:" << endl;
+    cin.getline(s, 101);
+    if (FirstLetter(s))
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+}
+
+bool FirstLetter(char* str) 
+{
+    int k = 0;
+    char* str2;
+
+    str2 = strtok(str, " "); // функція шукає розділення 
+    while (str2 != NULL) 
+    {
+        if (str2[0] == 'a') 
+            return true;
+
+        str2 = strtok(NULL, " ");
+    }
+    
+    return false;
+}
